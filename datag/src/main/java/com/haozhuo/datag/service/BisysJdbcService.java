fe90ee7dfe0cda072fe3fa7583222ab981069fb7
zhangyafang <zhangyafang@hz-health.cn>
2018-09-03 16:56:54 +0800

    新增风险评估bisys.prod_risk_evaluation接口

--- datag/src/main/java/com/haozhuo/datag/service/BisysJdbcService.java
+++ datag/src/main/java/com/haozhuo/datag/service/BisysJdbcService.java
@@ -1,13 +1,42 @@
 package com.haozhuo.datag.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by Lucius on 9/3/18.
  */
+@Component
 public class BisysJdbcService {
+    private static final Logger logger = LoggerFactory.getLogger(BisysJdbcService.class);
+
+    @Autowired
     @Qualifier("bisysJdbc") //选择jdbc连接池
     private  JdbcTemplate bisysDB;
+    public long getProdRiskEvaluation() {
+        long pv = 0;
+        try {
+            //当数据库中返回的数据为0条时，即查找不到这个用户时，这里会报错
+            pv = bisysDB.queryForObject(
+                    "select sum(pv) as pv from prod_risk_evaluation where trace_id = '首页_风险评估'",
+                    new RowMapper<Long>() {
+                        @Override
+                        public Long mapRow(ResultSet resultSet, int i) throws SQLException {
+                            return resultSet.getLong("pv");
+                        }
+                    });
+        } catch (Exception ex) {
+            logger.debug("getProdRiskEvaluation", ex);
+        }
+        return pv;
+    }
 
 }
--- datag/src/main/java/com/haozhuo/datag/web/BisysController.java
+++ datag/src/main/java/com/haozhuo/datag/web/BisysController.java
@@ -1,9 +1,11 @@
 package com.haozhuo.datag.web;
 
 import com.haozhuo.datag.service.BisysJdbcService;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Lucius on 9/3/18.
@@ -11,6 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/bisys")
 @RestController
 public class BisysController {
+    private static final Logger logger = LoggerFactory.getLogger(BisysController.class);
+
     @Autowired
     private BisysJdbcService bisysJdbcService;
+
+    @GetMapping("/ProdRiskEvaluation")
+    @ApiOperation(value = "直接调用接口", notes = "业务：查询prod_risk_evaluation的点击首页_风险评估的次数(PV，累加)")
+    public Object getProdRiskEvaluation() {
+        return bisysJdbcService.getProdRiskEvaluation();
+    }
+
 }
+
+

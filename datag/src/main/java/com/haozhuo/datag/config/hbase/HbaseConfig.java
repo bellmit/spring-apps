4da28c00f7c8300c9b99e5001988d2bcfc7e27ec
spring-apps <http://gitlab.ihaozhuo.com/Java_Service/spring-apps.git>
2019-07-09 20:01:56 +0800

    人体图接口hbase配置

---
+++ datag/src/main/java/com/haozhuo/datag/config/hbase/HbaseConfig.java
@@ -0,0 +1,44 @@
+package com.haozhuo.datag.config.hbase;
+
+
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.hadoop.hbase.HbaseTemplate;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+/**
+ * 把配置加入到hbaseTemplate中，交给spring容器
+ * 再把hbaseAdmin交给spring容器
+ */
+@Configuration
+//@EnableConfigurationProperties(HbaseProperties.class)
+public class HbaseConfig {
+
+@Bean
+public HbaseTemplate hbaseTemplate(@Value("${hbase.zookeeper.quorum}") String quorum,
+                                   @Value("${hbase.zookeeper.port}") String port,
+                                   @Value("${hbase.rpt.timeout}")  String rpctimeout,
+                                   @Value("${hbase.scanner.timeout.period}")  String ctimeout) {
+    HbaseTemplate hbaseTemplate = new HbaseTemplate();
+    org.apache.hadoop.conf.Configuration conf = HBaseConfiguration.create();
+    conf.set("hbase.zookeeper.quorum", quorum);
+    conf.set("hbase.zookeeper.port", port);
+    conf.set("hbase.rpt.timeout",rpctimeout);
+    conf.set("hbase.scanner.timeout.period",ctimeout);
+    conf.set("hbase.client.operation.timeout", "180000");
+    hbaseTemplate.setConfiguration(conf);
+    hbaseTemplate.setAutoFlush(true);
+    return hbaseTemplate;
+}
+
+}
+

8b3c1d20a4df6082b97d22c7d6281474577d9e6d
bingjie <http://gitlab.ihaozhuo.com/Java_Service/spring-apps.git>
2019-11-01 20:04:24 +0800

    保险

---
+++ datag/src/main/java/com/haozhuo/datag/model/report/PushBody.java
@@ -0,0 +1,31 @@
+package com.haozhuo.datag.model.report;
+
+public class PushBody {
+    private String rptid;
+    private String label;
+    private Integer age;
+
+    public String getRptid() {
+        return rptid;
+    }
+
+    public void setRptid(String rptid) {
+        this.rptid = rptid;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}

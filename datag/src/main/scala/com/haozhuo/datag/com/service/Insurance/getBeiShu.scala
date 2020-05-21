28ef4b0fa6ee571ba85ad80b5ad1ed7a78c63940
pts <pts2020@163.com>
2020-05-21 10:51:25 +0800

    fix:类型转换错误,清洗方法替换

--- datag/src/main/scala/com/haozhuo/datag/com/service/Insurance/getBeiShu.scala
+++ datag/src/main/scala/com/haozhuo/datag/com/service/Insurance/getBeiShu.scala
@@ -16,6 +16,92 @@ object getBeiShu {
     getBeiShu(78.9, filter("≤26"))
     println(d)
   }
+  def is_number(n:String)= {
+    var flag:Integer = 2
+    try{
+      n.toDouble
+      flag=1
+    }catch {
+      case ex: Exception =>
+        flag = 0
+    }
+    flag
+  }
+  def text_ref_replace(text_ref1:String)= {
+    var flag:Integer = 0
+    var text_ref_lower:String = ""
+    var text_ref_upper:String = ""
+    var text_ref = text_ref1.replace(" ","")
+    text_ref = text_ref.replace("-<=", "0.00-")
+    text_ref = text_ref.replace("-＜=", "0.00-")
+    text_ref = text_ref.replace("-<", "0.00-")
+    text_ref = text_ref.replace("-＜", "0.00-")
+
+    text_ref = text_ref.replace("<=", "0.00-")
+    text_ref = text_ref.replace("＜=", "0.00-")
+
+    text_ref = text_ref.replace("<", "0.00-")
+    text_ref = text_ref.replace("＜", "0.00-")
+    text_ref = text_ref.replace("〈", "0.00-")
+    text_ref = text_ref.replace("-≤", "0.00-")
+    text_ref = text_ref.replace("〈", "0.00-")
+    if(text_ref.startsWith("≤")){
+      text_ref = text_ref.replace("≤","0.00-")
+    }
+
+    if ( text_ref.contains("〉") ){
+      text_ref = text_ref.replace("〉", "")
+      text_ref = text_ref + "-Inf"
+    }
+    if ( text_ref.contains("->") ){
+      text_ref = text_ref.replace("〉", "")
+      text_ref = text_ref + "-Inf"
+    }
+    if ( text_ref.contains(">")) {
+      text_ref = text_ref.replace(">", "")
+      text_ref = text_ref + "-Inf"
+    }
+    if ( text_ref.contains("﹥"))
+      text_ref = text_ref.replace("﹥", "")
+    text_ref = text_ref + "-Inf"
+    if(text_ref.contains("＞")) {
+      text_ref = text_ref.replace("＞", "")
+      text_ref = text_ref + "-Inf"
+    }
+    if(text_ref.contains("》")) {
+      text_ref = text_ref.replace("》", "")
+      text_ref = text_ref + "-Inf"
+    }
+    if ( text_ref.contains("--")) text_ref = text_ref.replace("--", "-")
+    if ( text_ref.contains("～")) text_ref = text_ref.replace("～", "-")
+    if ( text_ref.contains("~")) text_ref = text_ref.replace("~", "-")
+    if (text_ref.contains("-")){
+      text_ref_lower = text_ref.split("-")(0)
+      text_ref_upper = text_ref.split("-")(1)
+      if (is_number(text_ref_lower)==1 && is_number(text_ref_upper)==1){
+        text_ref_lower = text_ref_lower.toDouble.formatted("%.2f").toString
+        if(text_ref_upper.equals("Inf")){
+        text_ref_upper = text_ref_upper
+        }else{
+          text_ref_upper = text_ref_upper.toDouble.formatted("%.2f").toString
+        }
+          text_ref = text_ref_lower + "-" + text_ref_upper
+      }
+      else
+      {
+        flag = 1
+        text_ref = text_ref1 + "(数据错误)"
+      }
+    }
+    else{
+      flag =2
+      text_ref = text_ref1 + "(数据错误)"
+    }
+
+    (text_ref,text_ref_lower,text_ref_upper,flag)
+  }
+
+
   def filterChaobiao(yourString: String) = {
     val regx = new Regex(
       """
@@ -62,16 +148,38 @@ object getBeiShu {
     }
   }
 
+  def getBeiShu(rs_val: Double, text_ref: String) = {
+    var result = rs_val
+    if(text_ref.isEmpty){
+      result=0
+    }else {
+      println(text_ref_replace(text_ref))
+      val min = text_ref_replace(text_ref)._2
+      val max = text_ref_replace(text_ref)._3
+      if (!min.equals("") && !max.equals("")) {
+        if (min.toDouble < rs_val && rs_val < max.toDouble) {
+          //println("合理范围")
+        } else if (rs_val >= max.toDouble) {
+          val d: Double = rs_val.toDouble / max.toDouble
+          result = d
+        } else if (rs_val < min.toDouble) {
+          val d: Double = rs_val / min.toDouble
+          result = d
+        }
+      }
+    }
+      result
+    }
 
 
   //参数描述:第一个你传递的值Double类型比如10,第二个是你传递的取值范围比如0.9-100
   //返回结果，如果是正常，则返回原始值，不正常，则返回倍数
-  def getBeiShu(rs_val: Double, text_ref: String) = {
+  def getBeiShu2(rs_val: Double, text_ref: String) = {
     var result = rs_val
 
     if(text_ref.isEmpty){
       result=0
-    }else{
+    }
 
 //1
     //判断<
@@ -93,8 +201,7 @@ object getBeiShu {
         val d: Double = rs_val / min
         result = d
       }
-    }
-    }//else结束
+    } //else结束
     result
   }
 }
